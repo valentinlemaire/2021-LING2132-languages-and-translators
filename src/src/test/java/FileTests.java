57893883558b78aaaad748dda41f52c748d8ca01
Augustindou <augustin.doultremontao@gmail.com>
2021-05-12 21:36:45 +0200

    uncommented commands to run

--- src/src/test/java/FileTests.java
+++ src/src/test/java/FileTests.java
@@ -2,11 +2,11 @@ public class FileTests {
     public static void main(String[] args) {
 
         String[][] commands = new String[][]{
-//                new String[]{"src/assets/fibonacci.ns", "50"},
-//                new String[]{"src/assets/fizzbuzz.ns"},
-//                new String[]{"src/assets/prime.ns", "50"},
-//                new String[]{"src/assets/sort.ns", "34", "56", "341", "1", "-34", "12", "44", "95", "103", "11", "12"},
-//                new String[]{"src/assets/uniq.ns", "hello", "bonjour", "ciao", "hello", "ciao", "coucou", "yo", "yo", "yo", "ciao"},
+                new String[]{"src/assets/fibonacci.ns", "50"},
+                new String[]{"src/assets/fizzbuzz.ns"},
+                new String[]{"src/assets/prime.ns", "50"},
+                new String[]{"src/assets/sort.ns", "34", "56", "341", "1", "-34", "12", "44", "95", "103", "11", "12"},
+                new String[]{"src/assets/uniq.ns", "hello", "bonjour", "ciao", "hello", "ciao", "coucou", "yo", "yo", "yo", "ciao"},
                 new String[]{"src/assets/extensions.ns", "10"}
         };
 
